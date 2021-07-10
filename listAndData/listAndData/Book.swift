7cfbc22886bdd265c292e44a39859e135a69339d
ian8829 <ghjkghj@naver.com>
2021-07-10 10:00:20 +0900

    add basic book model

---
+++ listAndData/listAndData/Book.swift
@@ -0,0 +1,16 @@
+//
+//  Book.swift
+//  listAndData
+//
+//  Created by ian white on 2021/07/10.
+//
+
+struct Book {
+    let title: String
+    let author: String
+    
+    init(title: String = "Title", author: String = "Author") {
+        self.title = title
+        self.author = author
+    }
+}
