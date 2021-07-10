d99e4eaef59e9905825a5c1ba4f1762e517e1701
ian8829 <ghjkghj@naver.com>
2021-07-10 09:54:55 +0900

    init status

---
+++ listAndData/listAndData/listAndDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  listAndDataApp.swift
+//  listAndData
+//
+//  Created by ian white on 2021/07/10.
+//
+
+import SwiftUI
+
+@main
+struct listAndDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
