3890090efba0ef09882379643bc305ced10f727d
ghjkghj <ghjkghj@naver.com>
2021-07-20 11:32:04 +0900

    add bookRow

--- listAndData/listAndData/Views/ContentView.swift
+++ listAndData/listAndData/Views/ContentView.swift
@@ -8,20 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        HStack {
-            Book.Image(title: Book().title)
-            
-            Text("Title")
-                .font(.title2)
-        }
-            
-    }
+	var body: some View {
+		BookRow(book: .init())
+	}
 }
+
+struct BookRow: View {
+	let book: Book
+	
+	var body: some View {
+		HStack {
+			Book.Image(title: book.title)
+			
+			VStack(alignment: .leading) {
+				Text(book.title)
+					.font(.title2)
+				Text(book.author)
+					.font(.title3)
+					.foregroundColor(.secondary)
+			}
+		}
+	}
+}
+
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+			ContentView()
+	}
 }
-
